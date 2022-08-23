e72c53169082ddd849560facde24949afef97451
Łukasz Sołysiński <lukasz.soltysinski@gft.com>
2022-08-23 22:41:54 +0200

    added test class

---
+++ src/test/java/com/teamproject/test/ExampleTest.java
@@ -0,0 +1,11 @@
+package com.teamproject.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ExampleTest {
+    @Test
+    public void exampleTest() {
+        Assert.assertTrue(true);
+    }
+}
